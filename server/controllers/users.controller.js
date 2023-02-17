90ff468c6eb34e8cc47bda30af23e376578f2309
Tiffany O <tiffanyortega29@gmail.com>
2023-02-17 01:02:56 -0600

    Back end log and reg complete

---
+++ server/controllers/users.controller.js
@@ -0,0 +1,66 @@
+const User = require('../models/users.model');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+module.exports = {
+
+    findAll: (req, res) => {
+        User.find()
+            .then(allUsers => res.json(allUsers))
+            .catch(err => res.status(400).json(err))
+    },
+
+    register: (req, res) => {
+        User.create(req.body)
+            .then(newUser => {
+                const userToken = jwt.sign({
+                    id: newUser._id
+                }, process.env.SECRET_KEY)
+
+                res
+                    .cookie("usertoken", userToken, { httpOnly:true })
+                    .json({ message:"SUCCESSFUL REGISTRATION", user: {
+                        id: newUser._id,
+                        firstName: newUser.firstName,
+                        lastName: newUser.lastName,
+                        username: newUser.username
+                    }});
+            })
+            .catch(err => {
+                console.log(err)
+                res.status(400).json({ message: "PROBLEM WITH REGISTRATION", error: err })
+            })
+    },
+
+    login: async (req, res) => {
+        const user = await User.findOne({ email:req.body.email })
+        if (user === null) {
+            return res.status(400).json({ message: "Invalid login" })
+        }
+
+        const correctPassword = await bcrypt.compare(req.body.password, user.password)
+        if (!correctPassword) {
+            return res.status(400).json({ message: "Invalid login" })
+        }
+
+        const userToken = jwt.sign({
+            id: user._id,
+        }, process.env.SECRET_KEY)
+        // console.log(userToken)
+
+        res
+            .cookie("usertoken", userToken, {
+                httpOnly: true
+            })
+            .json({ message: "Successful login", userInfo: {
+                id: user._id,
+                username: user.username
+            }});
+    },
+    
+    logout: (req, res) => {
+        res.clearCookie('usertoken');
+        res.sendStatus(200);
+    }
+}
+
---
+++ server/routes/users.routes.js
@@ -0,0 +1,9 @@
+const UserController = require('../controllers/users.controller');
+// const { authenticate } = require('../config/jwt.config');
+
+module.exports = (app) => {
+    app.get("/api/users", UserController.findAll);
+    app.post("/api/register", UserController.register);
+    app.post("/api/login", UserController.login);
+    app.get("/api/logout", UserController.logout);
+}
